e008a98dc8ef8e38cfe63695fc8ce6aac61862d8
Nikolai Shegai <nixer1978@yahoo.com>
2014-12-14 17:28:05 -0800

    DZ1
    ДЗ1

---
+++ addressbook-selenium-tests/src/ru/nixer/tests/GroupData.java
@@ -0,0 +1,15 @@
+package ru.nixer.tests;
+
+public class GroupData {
+	public String name;
+	public String header;
+	public String footer;
+
+	public GroupData() {
+	}
+	public GroupData(String groupname, String header, String footer) {
+		this.name = groupname;
+		this.header = header;
+		this.footer = footer;
+	}
+}
