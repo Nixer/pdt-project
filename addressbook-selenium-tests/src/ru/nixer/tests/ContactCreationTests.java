dea5d3ec49937929e6c9174cd7c9b47449780851
Nikolai Shegai <nixer1978@yahoo.com>
2015-01-19 16:05:55 -0800

    DZ6
    ДЗ6

--- addressbook-selenium-tests/src/ru/nixer/tests/ContactCreationTests.java
+++ addressbook-selenium-tests/src/ru/nixer/tests/ContactCreationTests.java
@@ -1,33 +1,54 @@
 package ru.nixer.tests;
 
+import java.util.Collections;
+import java.util.List;
+
+
+
 import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
 
 public class ContactCreationTests extends TestBase {	
   @Test
   public void testNonEmptyContactCreation() throws Exception {
     app.getNavigationHelper().openMainPage();
+    
+    //save old state
+    List<ContactData> oldList = app.getContactHelper().getContacts();
+    
+    //actions
     app.getContactHelper().initNewContactCreation();
-    ContactData group = new ContactData();
-    group.firstname = "Ivan";
-    group.lastname = "Ivanov";
-    group.address = "12345 Good Street";
-    group.home_phone = "123-4658";
-    group.mobile_phone = "123-458-78-89";
-    group.work_phone = "256-456-78";
-    group.email1 = "ivan@ibs.com";
-    group.email2 = "hun@hno.com";
-    group.bday = "4";
-    group.bmonth = "July";
-    group.byear = "1980";
-    group.group_name = "group 1";
-    group.address2 = "45684 Bass avw";
-    group.phone2 = "478-4549";
-   	app.getContactHelper().fillContactForm(group);  
+    ContactData contact = new ContactData();
+    contact.firstname = "Ivan";
+    contact.lastname = "Ivanov";
+    contact.address = "12345 Good Street";
+    contact.home_phone = "123-4658";
+    contact.mobile_phone = "123-458-78-89";
+    contact.work_phone = "256-456-78";
+    contact.email1 = "ivan@ibs.com";
+    contact.email2 = "hun@hno.com";
+    contact.bday = "4";
+    contact.bmonth = "July";
+    contact.byear = "1980";
+    contact.group_name = "group 1";
+    contact.address2 = "45684 Bass ave";
+    contact.phone2 = "478-4549";
+   	app.getContactHelper().fillContactForm(contact);  
     app.getContactHelper().submitContactCreation();
     app.getNavigationHelper().returnToHomePage();
+    
+    //save new state
+    List<ContactData> newList = app.getContactHelper().getContacts();
+    
+    //compare states        
+    oldList.add(contact);
+    Collections.sort(oldList);
+    Collections.sort(newList);
+    assertEquals(newList, oldList);
   }
   
-  @Test
+  //@Test
   public void testEmptyContactCreation() throws Exception {
     app.getNavigationHelper().openMainPage();
     app.getContactHelper().initNewContactCreation();
--- addressbook-selenium-tests/src/ru/nixer/tests/ContactData.java
+++ addressbook-selenium-tests/src/ru/nixer/tests/ContactData.java
@@ -1,6 +1,6 @@
 package ru.nixer.tests;
 
-public class ContactData {
+public class ContactData implements Comparable<ContactData> {
 	public String firstname;
 	public String lastname;
 	public String address;
@@ -38,4 +38,40 @@ public class ContactData {
 		this.address2 = address2;
 		this.phone2 = phone2;
 	}
+
+	@Override
+	public String toString() {
+		return "ContactData [firstname=" + firstname + "]";
+	}
+
+	@Override
+	public int hashCode() {
+	    final int prime = 31;
+		int result = 1;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ContactData other = (ContactData) obj;
+		if (firstname == null) {
+			if (other.firstname != null)
+				return false;
+		} else if (!firstname.equals(other.firstname))
+			return false;
+		return true;
+	}
+
+	@Override
+	public int compareTo(ContactData other) {
+		return this.firstname.toLowerCase().compareTo(other.firstname.toLowerCase());
+	}
+	
+		
 }
